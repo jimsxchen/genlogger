3376f3cff2c59903e8e21b69a3380523a0dfe11f
jimchen <jim.chen@dyson.com>
2022-03-03 09:06:41 +0000

    Fix bug of double 'not' and add round to time diff.

--- jlogger/j_heartbeat.py
+++ jlogger/j_heartbeat.py
@@ -6,7 +6,7 @@ class HeartbeatLog:
     """
     def __init__(self, name, interval_env):
         self._name = name
-        if not interval_env is not None and interval_env.isdigit():     # Note, interval_env must be positive integer number!
+        if interval_env is not None and interval_env.isdigit():     # Note, interval_env must be positive integer number!
             self._interval_val = int(interval_env)
         else:
             self._interval_val = 1
@@ -17,7 +17,7 @@ class HeartbeatLog:
             self._start_time = time.time()
 
         curr_time = time.time()
-        if int(curr_time-self._start_time) >= self._interval_val and logger != None:
+        if round(int(curr_time-self._start_time)) >= self._interval_val and logger != None:
             ori_fmtter = logger.root.handlers[0].formatter
             new_fmtter = logging.Formatter("%(asctime)s — " + self._name + " — %(funcName)s:%(lineno)d — %(levelname)s — %(message)s")
             logger.root.handlers[0].setFormatter(new_fmtter)
